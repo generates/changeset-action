48320c3123979269b79adc5f33423935cf5bae80
Ian Walter <pub@ianwalter.dev>
2020-07-12 15:44:27 -0400

    Adding conditional props

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const types = ['major', 'minor', 'patch']
 
 async function run () {
   // Try to extract changeset data from the workflow context.
-  const label = github.context.event.label.name
-  const title = github.context.event.pull_request.title
+  const label = github.context.event?.label?.name
+  const title = github.context.event?.pull_request?.title
 
   // Try tp extract changeset data from the pull request label.
   let [namespace = 'changeset', type] = label.split(':')
@@ -17,30 +17,29 @@ async function run () {
   type = type || core.getInput('type')
 
   if (namespace === 'changeset' && types.includes(type)) {
-    try {
-      // Get the package name from the workflow input or try to determine it by
-      // finding the nearest package.json to the first changed file.
-      let name = core.getInput('package')
-      if (!name) {
-        // TODO: implement
-      }
 
-      // Get the changeset summary from the workflow input or from the title of
-      // the pull request.
-      const summary = core.getInput('summary') || title
-      if (!summary) {
-        throw new Error('Changeset summary could not be determined')
-      }
-
-      // Try to write and commit the changeset.
-      await write({ summary, releases: [{ name, type }] })
-    } catch (err) {
-      core.error(err)
-      core.setFailed(err.message)
+    // Get the package name from the workflow input or try to determine it by
+    // finding the nearest package.json to the first changed file.
+    let name = core.getInput('package')
+    if (!name) {
+      // TODO: implement
     }
+
+    // Get the changeset summary from the workflow input or from the title of
+    // the pull request.
+    const summary = core.getInput('summary') || title
+    if (!summary) {
+      throw new Error('Changeset summary could not be determined')
+    }
+
+    // Try to write and commit the changeset.
+    await write({ summary, releases: [{ name, type }] })
   } else {
     core.info('Not adding changeset', { namespace, type })
   }
 }
 
-run()
+run().catch(err => {
+  core.error(err)
+  core.setFailed(err.message)
+})
