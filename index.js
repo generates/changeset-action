bbbdde2bf692eb5b3261a4560e62f60b9af226cf
Ian Walter <pub@ianwalter.dev>
2020-07-12 23:07:21 -0400

    Using github context instead of core

--- index.js
+++ index.js
@@ -8,33 +8,34 @@ const types = ['major', 'minor', 'patch']
 
 async function run () {
   // Try to extract changeset data from the workflow context.
-  const label = dot.get(github.context, 'event.label.name')
-  const title = dot.get(github.context, 'event.pull_request.title')
+  let { type, package, summary } = github.context.payload.inputs
+  let ns = 'changeset'
 
   // Try to extract changeset data from the pull request label or workflow
   // input.
-  print.debug('SUMARY', core.getInput('summary'), github.context)
-  let [ns, type] = label
-    ? label.split(':')
-    : ['changeset', core.getInput('type')]
+  const label = dot.get(github.context, 'event.label.name')
+  if (!type && label) {
+    const parts = label.split(':')
+    ns = parts[0]
+    type = parts[1]
+  }
 
   if (ns === 'changeset' && types.includes(type)) {
     // Get the package name from the workflow input or try to determine it by
     // finding the nearest package.json to the first changed file.
-    let name = core.getInput('package')
-    if (!name) {
+    if (!package) {
       // TODO: implement
     }
 
     // Get the changeset summary from the workflow input or from the title of
     // the pull request.
-    const summary = core.getInput('summary') || title
+    summary = summary || dot.get(github.context, 'event.pull_request.title')
     if (!summary) {
       throw new Error('Changeset summary could not be determined')
     }
 
     // Try to write and commit the changeset.
-    await write({ summary, releases: [{ name, type }] })
+    await write({ summary, releases: [{ name: package, type }] })
   } else {
     print.info('Not adding changeset', { ns, type })
   }
